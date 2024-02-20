960267f7b5ede6798440dca1bd034b9aaa586d25
muzaffer206 <muzaffer206@RPC08>
2024-02-20 11:50:47 +0300

    Proje dosyası ekle.

---
+++ Web Browser(ilkel)/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace Web_Browser_ilkel_
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.lb1 = new System.Windows.Forms.Label();
+            this.txtAdres = new System.Windows.Forms.TextBox();
+            this.btnGit = new System.Windows.Forms.Button();
+            this.btnAnaSayfa = new System.Windows.Forms.Button();
+            this.btnGeri = new System.Windows.Forms.Button();
+            this.btnİleri = new System.Windows.Forms.Button();
+            this.btnYenile = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnGecmis = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnGecmis);
+            this.panel1.Controls.Add(this.btnYenile);
+            this.panel1.Controls.Add(this.btnİleri);
+            this.panel1.Controls.Add(this.btnGeri);
+            this.panel1.Controls.Add(this.btnAnaSayfa);
+            this.panel1.Controls.Add(this.btnGit);
+            this.panel1.Controls.Add(this.txtAdres);
+            this.panel1.Controls.Add(this.lb1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(877, 47);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.webBrowser1);
+            this.panel2.Controls.Add(this.panel3);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 47);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(877, 403);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.listBox1);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel3.Location = new System.Drawing.Point(718, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(159, 403);
+            this.panel3.TabIndex = 0;
+            // 
+            // lb1
+            // 
+            this.lb1.AutoSize = true;
+            this.lb1.Location = new System.Drawing.Point(12, 15);
+            this.lb1.Name = "lb1";
+            this.lb1.Size = new System.Drawing.Size(40, 13);
+            this.lb1.TabIndex = 0;
+            this.lb1.Text = "Adres :";
+            // 
+            // txtAdres
+            // 
+            this.txtAdres.Location = new System.Drawing.Point(58, 12);
+            this.txtAdres.Name = "txtAdres";
+            this.txtAdres.Size = new System.Drawing.Size(320, 20);
+            this.txtAdres.TabIndex = 1;
+            // 
+            // btnGit
+            // 
+            this.btnGit.Location = new System.Drawing.Point(384, 10);
+            this.btnGit.Name = "btnGit";
+            this.btnGit.Size = new System.Drawing.Size(75, 23);
+            this.btnGit.TabIndex = 2;
+            this.btnGit.Text = "Git";
+            this.btnGit.UseVisualStyleBackColor = true;
+            this.btnGit.Click += new System.EventHandler(this.btnGit_Click);
+            // 
+            // btnAnaSayfa
+            // 
+            this.btnAnaSayfa.Location = new System.Drawing.Point(465, 10);
+            this.btnAnaSayfa.Name = "btnAnaSayfa";
+            this.btnAnaSayfa.Size = new System.Drawing.Size(75, 23);
+            this.btnAnaSayfa.TabIndex = 3;
+            this.btnAnaSayfa.Text = "Ana Sayfa";
+            this.btnAnaSayfa.UseVisualStyleBackColor = true;
+            this.btnAnaSayfa.Click += new System.EventHandler(this.btnAnaSayfa_Click);
+            // 
+            // btnGeri
+            // 
+            this.btnGeri.Location = new System.Drawing.Point(548, 10);
+            this.btnGeri.Name = "btnGeri";
+            this.btnGeri.Size = new System.Drawing.Size(75, 23);
+            this.btnGeri.TabIndex = 4;
+            this.btnGeri.Text = "Geri";
+            this.btnGeri.UseVisualStyleBackColor = true;
+            this.btnGeri.Click += new System.EventHandler(this.btnGeri_Click);
+            // 
+            // btnİleri
+            // 
+            this.btnİleri.Location = new System.Drawing.Point(629, 10);
+            this.btnİleri.Name = "btnİleri";
+            this.btnİleri.Size = new System.Drawing.Size(75, 23);
+            this.btnİleri.TabIndex = 5;
+            this.btnİleri.Text = "İleri";
+            this.btnİleri.UseVisualStyleBackColor = true;
+            this.btnİleri.Click += new System.EventHandler(this.btnİleri_Click);
+            // 
+            // btnYenile
+            // 
+            this.btnYenile.Location = new System.Drawing.Point(710, 10);
+            this.btnYenile.Name = "btnYenile";
+            this.btnYenile.Size = new System.Drawing.Size(75, 23);
+            this.btnYenile.TabIndex = 6;
+            this.btnYenile.Text = "Yenile";
+            this.btnYenile.UseVisualStyleBackColor = true;
+            this.btnYenile.Click += new System.EventHandler(this.btnYenile_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.ScriptErrorsSuppressed = true;
+            this.webBrowser1.Size = new System.Drawing.Size(718, 403);
+            this.webBrowser1.TabIndex = 1;
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            this.webBrowser1.Navigated += new System.Windows.Forms.WebBrowserNavigatedEventHandler(this.webBrowser1_Navigated);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(0, 0);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(159, 403);
+            this.listBox1.TabIndex = 0;
+            // 
+            // btnGecmis
+            // 
+            this.btnGecmis.Location = new System.Drawing.Point(792, 9);
+            this.btnGecmis.Name = "btnGecmis";
+            this.btnGecmis.Size = new System.Drawing.Size(75, 23);
+            this.btnGecmis.TabIndex = 7;
+            this.btnGecmis.Text = "Geçmiş";
+            this.btnGecmis.UseVisualStyleBackColor = true;
+            this.btnGecmis.Click += new System.EventHandler(this.btnGecmis_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(877, 450);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnYenile;
+        private System.Windows.Forms.Button btnİleri;
+        private System.Windows.Forms.Button btnGeri;
+        private System.Windows.Forms.Button btnAnaSayfa;
+        private System.Windows.Forms.Button btnGit;
+        private System.Windows.Forms.TextBox txtAdres;
+        private System.Windows.Forms.Label lb1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnGecmis;
+    }
+}
+
---
+++ Web Browser(ilkel)/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Web_Browser_ilkel_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGit_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate(txtAdres.Text);
+        }
+
+        private void btnAnaSayfa_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoHome();
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+
+        private void webBrowser1_Navigated(object sender, WebBrowserNavigatedEventArgs e)
+        {
+            listBox1.Items.Add(webBrowser1.Url);
+        }
+
+        private void btnGeri_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoBack();
+        }
+
+        private void btnİleri_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoForward();
+        }
+
+        private void btnYenile_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Refresh();
+        }
+
+        private void btnGecmis_Click(object sender, EventArgs e)
+        {
+            if (panel3.Visible) panel3.Visible = false;
+            else panel3.Visible = true;
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            panel3.Visible = false;
+        }
+    }
+}
